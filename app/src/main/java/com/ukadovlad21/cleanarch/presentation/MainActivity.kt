57f7405a4e55f8b1980a53836823822ada725629
Vlad Legkiy <ukadoV@gmail.com>
2023-01-19 22:23:36 +0200

    Use pattern MVVM

--- app/src/main/java/com/ukadovlad21/cleanarch/presentation/MainActivity.kt
+++ app/src/main/java/com/ukadovlad21/cleanarch/presentation/MainActivity.kt
@@ -2,9 +2,12 @@ package com.ukadovlad21.cleanarch.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.ukadovlad21.cleanarch.R
 import com.ukadovlad21.data.repository.UserRepositoryImpl
 import com.ukadovlad21.data.storage.sharedprefs.SharedPrefUserStorage
@@ -12,36 +15,31 @@ import com.ukadovlad21.domain.usecase.GetUserNameUseCase
 import com.ukadovlad21.domain.usecase.SaveUserNameUseCase
 
 class MainActivity : AppCompatActivity() {
-    private val userRepository by lazy(LazyThreadSafetyMode.NONE) {
-        UserRepositoryImpl(SharedPrefUserStorage(applicationContext))
-    }
-    private val getUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
-        GetUserNameUseCase(userRepository)
-    }
-    private val saveUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
-        SaveUserNameUseCase(userRepository)
-    }
+    private lateinit var vm: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.e("AAA", "Activity created")
+
+        vm = ViewModelProvider(this, MainViewModelFactory(this))[MainViewModel::class.java]
 
         val dataTextView = findViewById<TextView>(R.id.dataTextView)
         val dataEditText = findViewById<EditText>(R.id.dataEditText)
         val sendButton = findViewById<Button>(R.id.sendButton)
         val receiveButton = findViewById<Button>(R.id.receiveButton)
 
+        vm.resultLive.observe(this) {
+            dataTextView.text = it
+        }
 
         sendButton.setOnClickListener {
             val text = dataEditText.text.toString()
-            val params = com.ukadovlad21.domain.models.SaveUserNameParam(text)
-            val result = saveUserNameUseCase.execute(params)
-            dataTextView.text = "Save result = $result"
+            vm.save(text)
         }
         receiveButton.setOnClickListener {
-            val userName: com.ukadovlad21.domain.models.UserName = getUserNameUseCase.execute()
-            dataTextView.text = "${userName.firstname} ${userName.lastname}"
+            vm.load()
         }
 
 
---
+++ app/src/main/java/com/ukadovlad21/cleanarch/presentation/MainViewModel.kt
@@ -0,0 +1,45 @@
+package com.ukadovlad21.cleanarch.presentation
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.ukadovlad21.data.repository.UserRepositoryImpl
+import com.ukadovlad21.data.storage.sharedprefs.SharedPrefUserStorage
+import com.ukadovlad21.domain.usecase.GetUserNameUseCase
+import com.ukadovlad21.domain.usecase.SaveUserNameUseCase
+
+class MainViewModel(
+    private val getUserNameUseCase: GetUserNameUseCase,
+    private val saveUserNameUseCase: SaveUserNameUseCase,
+) : ViewModel() {
+
+    private var resultLiveMutable = MutableLiveData<String>()
+
+    val resultLive: LiveData<String> = resultLiveMutable
+
+    init {
+        Log.e("AAA", "VM Created")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.e("AAA", "VM cleared")
+    }
+
+//    fun getResultLive():LiveData<String> {
+//        return resultLive
+//    }
+
+    fun save(text: String) {
+        val params = com.ukadovlad21.domain.models.SaveUserNameParam(text)
+        val resultData = saveUserNameUseCase.execute(params)
+        resultLiveMutable.value = "Save result = $resultData"
+    }
+
+
+    fun load() {
+        val userName: com.ukadovlad21.domain.models.UserName = getUserNameUseCase.execute()
+        resultLiveMutable.value = "${userName.firstname} ${userName.lastname}"
+    }
+}
